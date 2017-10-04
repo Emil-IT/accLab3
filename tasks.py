75c9313124b722cd879e35380765b7a2c5adb8df
Emil-IT <emil.osterberg90@gmail.com>
2017-10-04 16:38:20 +0000

    Some test files

--- tasks.py
+++ tasks.py
@@ -1,7 +1,20 @@
-from celery import Celery
+from flask import Flask
+from flask_celery import make_celery
 
-app = Celery('tasks', backend='rpc://', broker='amqp://')
+flask_app = Flask(__name__)
+flask_app.config['CELERY_BROKER_URL']='amqp://localhost//'
+flask_app.config['CELERY_BACKEND']='rpc://'
 
-@app.task
+celery = make_celery(flask_app)
+
+@flask_app.route('/hello/')
+def hello():
+	print('Recieved message')
+	add.delay(2,5)
+	return 'Request sent'
+
+@celery.task(name='tasks.add')
 def add(x, y):
 	return x + y
+if __name__ == '__main__':
+	flask_app.run(debug=True)
