b0a95c5e491d9f2877225fdcedfecf1723a1123d
emil-it <emil.osterberg90@gmail.com>
2017-10-05 20:22:40 +0000

    Improved pronoum finder

--- count_words.py
+++ count_words.py
@@ -1,5 +1,6 @@
-import os,json
+import os,json,re
 def countPronouns():
+	regex = re.compile('[^a-zA-Z]')
 	pronouns = {'han':0,'hon':0,'hen':0,'den':0,'det':0,'denna':0,'denne':0}
 	for twitterFile in os.listdir('./data'):
 		with open('./data/' + twitterFile,'rw') as file:
@@ -9,6 +10,7 @@ def countPronouns():
 					if not 'retweeted_status' in data:
 						words = data['text'].lower().split()
 						for word in words:
+							word = regex.sub('',word)
 							if word in pronouns:
 								pronouns[word]+=1
 	return(pronouns)
